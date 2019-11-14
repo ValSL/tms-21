22fc27f785149b9ebe4058ca019dd66340c79d14
ValSL <valid.saley@gmail.com>
2019-11-14 21:13:05 +0300

    matrix create with default arguments reformat

--- cw07_03.py
+++ cw07_03.py
@@ -1,5 +1,6 @@
 import random
 
+
 def matrix_create(n, rand_from = 1, rand_to = 9):
     matrix = []
     for row in range(n):
@@ -8,5 +9,6 @@ def matrix_create(n, rand_from = 1, rand_to = 9):
             mass_line.append(random.randint(rand_from, rand_to))
         matrix.append(mass_line)
     return matrix
+
 
 print(matrix_create(3,1,20))
