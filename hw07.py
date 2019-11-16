820ca4a20b4a8c2461433baeac13403ec669d6f0
ValSL <valid.saley@gmail.com>
2019-11-16 19:06:21 +0300

    inches_to_centimeters

---
+++ hw07.py
@@ -0,0 +1,16 @@
+'''Написать 12 функций по переводу:'''
+
+'''Дюймы в сантиметры'''
+
+
+def inches_to_centimeters(inches):
+    cent = inches * 2.54
+    return cent
+
+
+a = inches_to_centimeters(54)
+print(a)
+
+
+
+
