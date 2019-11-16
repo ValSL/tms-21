1e51359cf396acf8fc787aa81b3573c747e349a8
ValSL <valid.saley@gmail.com>
2019-11-16 19:07:22 +0300

    centimeters_to_inches

--- hw07.py
+++ hw07.py
@@ -1,8 +1,6 @@
 '''Написать 12 функций по переводу:'''
 
 '''Дюймы в сантиметры'''
-
-
 def inches_to_centimeters(inches):
     cent = inches * 2.54
     return cent
@@ -12,5 +10,12 @@ a = inches_to_centimeters(54)
 print(a)
 
 
+'''Сантиметры в дюймы'''
+def centimeters_to_inches(cent):
+    inch = cent * 0.39
+    return inch
 
+
+a = centimeters_to_inches(220)
+print(a)
 
