14672853a598fda12be95a208e6f6e809292abcb
ValSL <valid.saley@gmail.com>
2019-11-18 19:57:21 +0300

    8.2 ref

--- cw08_task02.py
+++ cw08_task02.py
@@ -1,4 +1,7 @@
-cars = [{'serial_number': 1, 'year': 1998}, {'serial_number': 2, 'year': 2001}]
+cars = [
+    {'serial_number': 1, 'year': 1998},
+    {'serial_number': 2, 'year': 2001},
+]
 
 list02 = [car for car in cars if car['year'] > 2000]
 
