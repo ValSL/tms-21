8acbd33a3c42271acaeb20ce6fe42d567ae7105c
ValSL <valid.saley@gmail.com>
2019-11-19 11:44:15 +0300

    hw08 task01

---
+++ hw08_task01.py
@@ -0,0 +1,29 @@
+'''Описать функцию fact2( n ) вещественного типа, вычисляющую двойной факториал :
+n!! = 1·3·5·...·n, если n — нечетное; n!! = 2·4·6·...·n, если n — четное
+(n > 0 — параметр целого типа.
+С помощью этой функции найти двойные факториалы пяти данных целых чисел [01-11.2-Proc35]
+'''
+
+
+def fact2(n):
+    res = 1
+    if n % 2 == 0:
+        even_list = [i for i in range(1, n+1) if i % 2 == 0]
+        for i in even_list:
+            res = res * i
+        return res
+    if not n % 2 == 0:
+        not_even_list = [i for i in range(1, n + 1) if not i % 2 == 0]
+        for i in not_even_list:
+            res = res * i
+        return res
+
+
+def main():
+    a = fact2(7)
+    print(a)
+
+
+if __name__ == '__main__':
+    main()
+
