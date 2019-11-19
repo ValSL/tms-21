58c07db811e3f9002c7583526bc8c9f0beb4d928
ValSL <valid.saley@gmail.com>
2019-11-19 13:56:34 +0300

    hw08 task03 temp2

--- hw_task03.py
+++ hw_task03.py
@@ -1,14 +1,19 @@
 def fact(number):
     res = 1
-    for i in range(number + 1):
+    for i in range(1, number + 1):
         res = i * res
     return res
 
-# def sin1(x, e):
-#     sin_x = ((-1)** n) * (x**((2*n+1)/(2*n+1)))
+
+def sin1(x, e):
+    sin_x = 1
+    for n in range(1, 6):
+        sin_x += ((-1)**n) * (((-1)** n) * (x**((2*n+1)/(fact(2*n+1)))))
+    return sin_x
+
 
 def main():
-    print(fact(5))
+    print(sin1(1,3))
 
 
 if __name__ == "__main__":
