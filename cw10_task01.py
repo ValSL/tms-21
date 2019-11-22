4fa1080dc2e2b708aa829fef6e868dac922b3214
ValSL <valid.saley@gmail.com>
2019-11-22 20:18:55 +0300

    cw10 task01

--- cw10_task01.py
+++ cw10_task01.py
@@ -3,7 +3,8 @@ def print_first_line():
     print(f.readline())
     f.close()
 
-def prinf_fifth_line():
+
+def print_fifth_line():
     f = open('test.txt')
     for i in range(1, 6):
         f.readline()
@@ -14,8 +15,29 @@ def prinf_fifth_line():
 
 def print_five_lines():
     f = open('test.txt')
-    for line in range(1, 5):
-        print(f.readline())
+    for line in range(1, 6):
+        print(f.readline().strip())
     f.close()
 
 
+def print_certain_lines(a, b):
+    f = open('test.txt')
+    index = 1
+    counter = 0
+    while True:
+        line = f.readline()
+        counter += 1
+        if not line:
+            break
+        if b >= counter >= a:
+            print(line)
+    f.close()
+
+
+def print_all_file():
+    f = open('test.txt')
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        print(line)
