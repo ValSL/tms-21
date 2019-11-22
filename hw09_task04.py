f2c725d8ca67b364feeb9a60551f558cf4802af9
ValSL <valid.saley@gmail.com>
2019-11-22 19:48:14 +0300

    hw09 task04 fix

--- hw09_task04.py
+++ hw09_task04.py
@@ -2,8 +2,8 @@
 
 
 def argument_reverse_decorator(func):
-    def argument_rev(a, b):
-        func(b, a)
+    def argument_rev(*args):
+        func(*args[::-1])
     return argument_rev
 
 
@@ -12,9 +12,15 @@ def calc(a, b):
     res = a - b
     print(res)
 
+@argument_reverse_decorator
+def calc5(a, b, c, d, e):
+    res = a - b - c - d - e
+    print(res)
+
 
 def main():
     calc(6, 2)
+    calc5(20, 4, 3, 2, 1)
 
 
 if __name__ == '__main__':
