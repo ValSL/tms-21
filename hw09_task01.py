0905b6b86dbcce489b6ee1a723a5159fcf1d3a1f
ValSL <valid.saley@gmail.com>
2019-11-22 16:56:17 +0300

    hw09 task01

---
+++ hw09_task01.py
@@ -0,0 +1,12 @@
+'''Дан список строк.
+Отформатировать все строки в формате ‘{i} - {string}’, где i это порядковый номер строки в списке.
+Использовать генератор списков.'''
+def main():
+    strings = ['first', 'second', 'third']
+
+    new_lst = [f'{string_index + 1} - {string}' for string_index, string in enumerate(strings)]
+    print(new_lst)
+
+
+if __name__ == '__main__':
+    main()
