c47d3d4a212de4d0d95d6bacc819c7be09d10302
ValSL <valid.saley@gmail.com>
2019-11-25 20:44:10 +0300

    cw10 task04

---
+++ cw10_task04/cw10_task04.py
@@ -0,0 +1,18 @@
+def main():
+    with open('task04.txt', 'r') as f:
+        file_1 = f.readlines()
+
+        for line in file_1:
+            new_line = ''
+            for index, elem in enumerate(line):
+                if elem == '0':
+                    new_line = new_line + '1'
+                elif elem == '1':
+                    new_line = new_line + '0'
+
+            f2 = open('new_file', 'a')
+            f2.write(f'{new_line}\n')
+
+
+if __name__ == '__main__':
+    main()
