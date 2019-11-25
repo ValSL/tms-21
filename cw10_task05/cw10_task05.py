92a8e2b49350debe3cda04316a1b752a871ad330
ValSL <valid.saley@gmail.com>
2019-11-25 21:12:29 +0300

    cw10 task05

---
+++ cw10_task05/cw10_task05.py
@@ -0,0 +1,15 @@
+def main():
+    with open('task05.txt', 'r') as f:
+        for index, line in enumerate(f.readlines()):
+            if (index + 1) % 2 == 0:
+                f2 = open('file2.txt', 'a')
+                f2.write(f'{line}\n')
+                f2.close()
+            elif (index + 1) % 2 != 0:
+                f3 = open('file3.txt', 'a')
+                f3.write(f'{line}\n')
+                f3.close()
+
+
+if __name__ == '__main__':
+    main()
