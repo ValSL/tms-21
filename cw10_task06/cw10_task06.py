87cb38b8ca258d561f150c6d22fbc4db58eecb68
ValSL <valid.saley@gmail.com>
2019-11-25 21:40:59 +0300

    task 07

---
+++ cw10_task06/cw10_task06.py
@@ -0,0 +1,26 @@
+from random import randint
+import json
+
+
+def main():
+    matrix = []
+    for i in range(3):
+        line = []
+        for j in range(3):
+            line.append(randint(1,9))
+        matrix.append(line)
+
+    with open('file.json', 'a') as f:
+        data = json.dumps({'matrix': matrix})
+        f.write(data)
+
+    with open('file2.json', 'w'):
+        for row in matrix:
+            for index, elem in enumerate(row):
+                if elem % 2 == 0:
+                    matrix[index] = 0
+        data2 = json.dumps({'matrix': matrix})
+
+
+if __name__ == '__main__':
+    main()
