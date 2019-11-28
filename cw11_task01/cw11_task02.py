4a6af463b6206c69945e48b999ba8843432002ab
ValSL <valid.saley@gmail.com>
2019-11-28 21:34:56 +0300

    Nasl

--- cw11_task01/cw11_task02.py
+++ cw11_task01/cw11_task02.py
@@ -1,64 +1,33 @@
-class Dog:
+class Pet:
     def __init__(self, name, age, master):
         self.name = name
         self.age = age
         self.master = master
 
+    def run(self):
+        print('run')
+
+    def jump(self):
+        print('jump')
+
+    def sleep(self):
+        print('sleep')
+
+    def birthday(self):
+        self.age += 1
+
+
+class Dog(Pet):
     def bark(self):
         print('bark')
 
-    def run(self):
-        print('run')
 
-    def jump(self):
-        print('jump')
-
-    def sleep(self):
-        print('sleep')
-
-    def birthday(self):
-        self.age += 1
-
-
-class Cat:
-    def __init__(self, name, age, master):
-        self.name = name
-        self.age = age
-        self.master = master
-
+class Cat(Pet):
     def meow(self):
         print('meow')
 
-    def run(self):
-        print('run')
 
-    def jump(self):
-        print('jump')
-
-    def sleep(self):
-        print('sleep')
-
-    def birthday(self):
-        self.age += 1
-
-
-class Parrot:
-    def __init__(self, name, age, master):
-        self.name = name
-        self.age = age
-        self.master = master
-
+class Parrot(Pet):
     def fly(self):
         print('fly')
 
-    def run(self):
-        print('run')
-
-    def jump(self):
-        print('jump')
-
-    def sleep(self):
-        print('sleep')
-
-    def birthday(self):
-        self.age += 1
