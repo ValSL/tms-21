a64fc5545e288d04ad405c54c53636655985e3fc
ValSL <valid.saley@gmail.com>
2019-11-28 19:59:06 +0300

    task03 methods

--- cw11_task01/cw11.py
+++ cw11_task01/cw11.py
@@ -1,11 +1,18 @@
 class Dog:
-    pass
+    def jump(self):
+        print('Jump')
+
+    def run(self):
+        print('Run')
 
 
 def main():
     dog1 = Dog()
     dog2 = Dog()
     print(dog1, dog2)
+    print('______________')
+    dog1.jump()
+    dog1.run()
 
 
 if __name__ == '__main__':
