1f8f279bd2d37192665c4dec0956cf8dd1f5a9d8
ValSL <valid.saley@gmail.com>
2019-11-28 20:51:20 +0300

    private args

--- cw11_task01/cw11.py
+++ cw11_task01/cw11.py
@@ -1,9 +1,13 @@
 class Dog:
-    def __init__(self, height, weight, name, age):
+    def __init__(self, height, weight, name, age, master):
+        self.__master = master
         self.height = height
         self.weight = weight
         self.name = name
         self.age = age
+
+    def get_master(self):
+        return self.__master
 
     def change_height(self, height):
         self.height = height
@@ -22,7 +26,7 @@ class Dog:
 
 
 def main():
-    dog1 = Dog(2, 50, 'Barsik', 5)
+    dog1 = Dog(2, 50, 'Barsik', 5, 'Vovchik')
     print(dog1)
     print('______________')
 
@@ -35,6 +39,11 @@ def main():
     print(dog1.name)
     dog1.change_name('Bobik')
     print(dog1.name)
+
+
+    print(dog1._Dog__master)
+    a = dog1.get_master()
+    print(a)
 
 
 if __name__ == '__main__':
