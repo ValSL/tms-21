9b9697a500a40251e849eca44ccc90024fad12c6
ValSL <valid.saley@gmail.com>
2019-11-28 21:21:49 +0300

    decorators

--- cw11_task01/cw11.py
+++ cw11_task01/cw11.py
@@ -2,28 +2,58 @@ class Dog:
     def __init__(self, height, weight, name, age, master, city='Minsk'):
         self.__city = city
         self.__master = master
-        self.height = height
-        self.weight = weight
-        self.name = name
-        self.age = age
+        self.__height = height
+        self.__weight = weight
+        self.__name = name
+        self.__age = age
 
-    def get_master(self):
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, weight):
+        self.__weight = weight
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        self.__age = age
+
+    @property
+    def master(self):
         return self.__master
 
-    def set_master(self, master):
+    @master.setter
+    def master(self, master):
         self.__master = master
 
-    def get_city(self):
+    @property
+    def city(self):
         return self.__city
 
-    def set_city(self, city):
+    @city.setter
+    def city(self, city):
         self.__city = city
-
-    def change_height(self, height):
-        self.height = height
-
-    def change_name(self, name):
-        self.name = name
 
     def bark(self):
         print('Woof Woof!')
