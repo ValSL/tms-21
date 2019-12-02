406c9e97a49107fee0d5dfca0301ef79f70db5c5
ValSL <valid.saley@gmail.com>
2019-12-02 19:51:46 +0300

    jump

--- cw11_task01/cw11_task02.py
+++ cw11_task01/cw11_task02.py
@@ -21,8 +21,8 @@ class Pet:
     def run(self):
         print('run')
 
-    def jump(self):
-        print('jump')
+    def jump(self, x):
+        print(f'jump {x} meters')
 
     def sleep(self):
         print('sleep')
@@ -35,10 +35,22 @@ class Dog(Pet):
     def bark(self):
         print('bark')
 
+    def jump(self, x):
+        if x > 0.5:
+            print('Dogs cannot jump so high!')
+        else:
+            super().jump(x)
+
 
 class Cat(Pet):
     def meow(self):
         print('meow')
+
+    def jump(self, x):
+        if x > 2:
+            print('Cats cannot jump so high!')
+        else:
+            super().jump(x)
 
 
 class Parrot(Pet):
@@ -48,23 +60,29 @@ class Parrot(Pet):
         else:
             print('fly')
 
-    def change_weight(self, weight=None):
-        if weight:
-            self.weight += weight
-        else:
-            self.weight += 0.5
+    def change_weight(self, weight=0.05):
+        self.weight += weight
 
-    def change_height(self, height=None):
-        if height:
-            self.weight += height
-        else:
-            self.weight += 0.5
+    def change_height(self, height=0.05):
+        self.weight += height
 
+    def jump(self, x):
+        if x > 0.05:
+            print('Parrots cannot jump so high!')
+        else:
+            super().jump(x)
 
 
 parrot = Parrot('Vovchik', 3, 'Pasha', 0.01, 4)
+dog = Dog('Bobik', 3, 'vovchik', 4, 5)
+cat = Cat('Murzik', 5, 'Pasha', 5, 6)
 parrot.change_weight()
 print(parrot.weight)
 parrot.fly()
+print('_____________________________')
+dog.jump(3)
+parrot.jump(0.01)
+cat.jump(2)
+
 
 
