9c6ea93123348cd0a9b4c598831f71adcfe6b4e2
ValSL <valid.saley@gmail.com>
2019-12-10 18:45:56 +0300

    cw14_06

--- cw14/cw14_06.py
+++ cw14/cw14_06.py
@@ -2,6 +2,9 @@ import os, argparse, sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-dn', '--dir_name', default='new_dir')
+parser.add_argument('-fn', '--file_name', default='new_file')
 args = parser.parse_args()
 current_dir = os.getcwd()
 os.mkdir(f'{current_dir}/{args.dir_name}')
+with open(f'{current_dir}/{args.dir_name}/{args.file_name}.txt', 'w') as f:
+    f.write('')
