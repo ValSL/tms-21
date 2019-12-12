4611a55a45c4f7466f495c80ad82b96e8ce32287
ValSL <valid.saley@gmail.com>
2019-12-12 21:17:34 +0300

    data insert 2

--- cw15_02/cw15_02.py
+++ cw15_02/cw15_02.py
@@ -3,6 +3,6 @@ from sqlalchemy import create_engine
 e = create_engine("sqlite:///test.db")
 e.execute("""
    insert into user (firstname, lastname)
-       values ('Valid', 'Saley')
+       values ('Pasha', 'Pashov'), ('Vova', 'Vovov'), ('Ivan', 'Ivanov'), ('Andrey', 'Andreev')
 """)
 
