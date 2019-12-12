06f5df942f254716a4efd0002790de7a5cbd0926
ValSL <valid.saley@gmail.com>
2019-12-12 21:41:46 +0300

    sesion

--- cw15_02/cw15_05.py
+++ cw15_02/cw15_05.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
@@ -5,13 +6,24 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine("sqlite:///test.db", echo=True)
 metadata = MetaData()
 Base = declarative_base()
-class Person(Base):
-   __tablename__ = 'person'
-   id = Column(Integer, primary_key=True)
-   firstname = Column(String)
-   lastname = Column(String)
 
-   def __init__(self, firstname, lastname):
-       self.firstname = firstname
-       self.lastname = lastname
+
+class Person(Base):
+    __tablename__ = 'person'
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String)
+    lastname = Column(String)
+
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+
 Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+session.add(Person('Alex', 'Varkalov'))
+session.add_all([Person('Alex', 'Petrov'), Person('Ann', 'Ivanova')])
+session.commit()
+
